7b72bd48780bcd6b112f45c5de305d78a86c4862
lawsonvt <mlawsonvt09@gmail.com>
2025-10-03 11:11:43 -0400

    Redoing 3D adjacency matrix based on feedback

--- src/cell_to_cell_adjacency_3d.r
+++ src/cell_to_cell_adjacency_3d.r
@@ -15,7 +15,7 @@ if(!genv_exists){
   installGiottoEnvironment()
 }
 
-out_dir <- "results/giotto_spatial_experiment/"
+out_dir <- "results/cell_to_cell_adjacency_3d/"
 dir.create(out_dir, recursive = T, showWarnings = F)
 
 results_folder = out_dir
@@ -32,6 +32,7 @@ instrs <- createGiottoInstructions(
 m3d_se <- readRDS("../WT_v_APP_PS19/results/anndata_objects/all_cells_integrated.spatial_exp.RDS")
 # pull out cell metadata
 cell_metadata <- as.data.frame(colData(m3d_se))
+cell_metadata$cell_id <- rownames(cell_metadata)
 
 cell_type_counts <- as.data.frame(table(cell_metadata$Cell.Type))
 cell_type_counts <- cell_type_counts[order(cell_type_counts$Freq),]
@@ -53,78 +54,23 @@ m3d_g@spatial_locs$cell$raw$sdimz <- spatialCoords(m3d_se)[,"Z"]
 # plot instructions for Giotto
 instructions(m3d_g) <- instrs
 
-# NOTE: THIS DOES NOT WORK
-# plotUMAP_3D(m3d_g,
-#             cell_color = "Cell.Type", 
-#             show_center_label = FALSE,
-#             dim_reduction_name = "X_umap",
-#             save_param = list(save_name = "umap_leiden.cell_types")
-# )
+# make a sample name that has the condition in it included
+cell_metadata$sample_cond <- paste0(cell_metadata$ID, " ",cell_metadata$Condition)
 
-# colors for consistent plotting
-cell_colors <- metadata(m3d_se)$"Cell Type_colors"
+sample_ids <- unique(cell_metadata$sample_cond)
 
-# 2D plot
-spatPlot2D(m3d_g,
-           cell_color="Cell.Type",
-           cell_color_code=cell_colors,
-           point_size = 1,
-           save_param = list(save_name = "integrated.2d_splot"))
-
-# 3D plot with real axes
-spatPlot3D(m3d_g,
-           cell_color="Cell.Type",
-           axis_scale = "real",
-           cell_color_code=cell_colors,
-           point_size = 2,
-           save_param = list(save_name = "integrated.3d_splot_real"))
-
-# 3D plot with equal sized axes
-spatPlot3D(m3d_g,
-           cell_color="Cell.Type",
-           cell_color_code=cell_colors,
-           save_param = list(save_name = "integrated.3d_splot"))
-
-
-# create 2d plots per sample
-sample_ids <- levels(cell_metadata$ID)
+sample_order <- c("C164B WT","C158B APPPS19",
+                  "C166A WT", "C165 APPPS19")
 
 # separate out samples by subsetting
 m3d_samples <- lapply(sample_ids, function(id) {
   
-  cell_ids <- rownames(cell_metadata[cell_metadata$ID == id,])
+  cell_ids <- rownames(cell_metadata[cell_metadata$sample_cond == id,])
   
   subset(m3d_g, cell_ids=cell_ids)
   
 })
 names(m3d_samples) <- sample_ids
-
-# make 2D plots per sample
-for (id in sample_ids) {
-  
-  print(id)
-  # 2D plot
-  spatPlot2D(m3d_samples[[id]],
-             cell_color="Cell.Type",
-             cell_color_code=cell_colors,
-             title=id,
-             point_size = 1,
-             save_param=list(save_name=paste0(id, ".2d_splot")))
-  # 3D plot with equal sized axes
-  spatPlot3D(m3d_samples[[id]],
-             cell_color="Cell.Type",
-             cell_color_code=cell_colors,
-             title=id,
-             save_param=list(save_name=paste0(id, ".3d_splot")))
-  # 3D plot with real axes
-  spatPlot3D(m3d_samples[[id]],
-             cell_color="Cell.Type",
-             cell_color_code=cell_colors,
-             axis_scale="real",
-             title=id,
-             save_param=list(save_name=paste0(id, ".3d_splot_real")))
-  
-}
 
 # connection analysis per sample
 cell_connections <- lapply(sample_ids, function(id) {
@@ -135,8 +81,8 @@ cell_connections <- lapply(sample_ids, function(id) {
   
   # use Delaundy triangulation to determine spatial network
   g <- createSpatialNetwork(g, method = "Delaunay",
-                       delaunay_method = "delaunayn_geometry",
-                       name = "spatial_network")
+                            delaunay_method = "delaunayn_geometry",
+                            name = "spatial_network")
   
   # pull out spatial network
   sp_net <- g@spatial_network$cell$spatial_network
@@ -155,24 +101,21 @@ cell_connections <- lapply(sample_ids, function(id) {
   
   
   # merge in metadata to determine cell types
-  cell_metadata$cell_id <- rownames(cell_metadata)
-  
   sp_graph_simple_both <- merge(sp_graph_simple_both,
-                                cell_metadata[,c("cell_id","Cell.Type")],
+                                cell_metadata[,c("cell_id","Cell.Type","Condition")],
                                 by.x="from",
                                 by.y="cell_id")
   
   sp_graph_simple_both <- merge(sp_graph_simple_both,
-                                cell_metadata[,c("cell_id","Cell.Type")],
+                                cell_metadata[,c("cell_id","Cell.Type", "Condition")],
                                 by.x="to",
                                 by.y="cell_id",
                                 suffixes=c("_from","_to"))
   
-
+  
   return(sp_graph_simple_both)
 })
 names(cell_connections) <- sample_ids
-
 
 # count up totals per cell
 cell_connect_totals <- lapply(sample_ids, function(id) {
@@ -181,21 +124,19 @@ cell_connect_totals <- lapply(sample_ids, function(id) {
   
   # sum up connected cell count
   cell_connect_total <- ddply(cell_connect,
-                          .(from, Cell.Type_from),
-                          summarise,
-                          cell_count=length(unique(to)))
+                              .(from, Cell.Type_from),
+                              summarise,
+                              cell_count=length(unique(to)))
   # add in sample metadata
-  cell_connect_total$sample_id <- id
-  cell_connect_total$sample_cond <- paste0(id, " (",
-                                       unique(cell_metadata[cell_metadata$ID == id,]$Condition), ")")
-
+  cell_connect_total$sample <- id
+  
   return(cell_connect_total)
 })
 cell_connect_totals <- bind_rows(cell_connect_totals)
 
 # calc means and medians
 cell_connect_stats <- ddply(cell_connect_totals,
-                            .(sample_id),
+                            .(sample),
                             summarise,
                             mean=mean(cell_count),
                             median=median(cell_count),
@@ -205,6 +146,11 @@ cell_connect_stats$stat <- paste0("Mean: ", round(cell_connect_stats$mean, digit
                                   "\nMedian: ", round(cell_connect_stats$median, digits=2),
                                   "\nStdDev: ", round(cell_connect_stats$sd, digits=2))
 
+cell_connect_totals$sample <- factor(cell_connect_totals$sample,
+                                     levels=sample_order)
+cell_connect_stats$sample <- factor(cell_connect_stats$sample,
+                                    levels=sample_order)
+
 # plot histogram of counts across samples
 ggplot(cell_connect_totals,
        aes(cell_count)) +
@@ -212,19 +158,63 @@ ggplot(cell_connect_totals,
   theme_bw() +
   labs(x="Number of Connected Cells",
        y="Frequency") +
-  facet_wrap(~ sample_cond, ncol=2) +
+  facet_wrap(~ sample, ncol=2) +
   scale_x_continuous(breaks=c(0,5,10,15,20,25,30, 35, 40)) +
   geom_label(data=cell_connect_stats, aes(label=stat), x=30, y=3100, size=3)
 ggsave(paste0(out_dir, "sample_cell_connectivity.histogram.png"), width=6, height=5)
 
-# create adjacency matrices
+
+# make one big adjacency matrix, to determine cell type order
+cell_connections_big <- bind_rows(cell_connections)
+
+cell_type_connect_big <- ddply(cell_connections_big,
+                               .(Cell.Type_from,Cell.Type_to),
+                               summarise,
+                               cell_count=length(from))  
+
+cell_type_connect_big$scaled_count <- scale(log(cell_type_connect_big$cell_count))
+
+connect_big_matrix <- acast(cell_type_connect_big,
+                            Cell.Type_from ~ Cell.Type_to,
+                            value.var = "scaled_count")
+
+pdf(paste0(out_dir, "all_samples.cell_type_adj_matrix.pdf"), width=8, height=7)
+print(Heatmap(connect_big_matrix,
+              col=colorRamp2(c(-1.5,0,1.5), c("blue","white","red")),
+              name = "Normalized\nEdge Count",
+              column_title="All Samples"))
+dev.off()
+
+# hard code cell type order across samples
+cell_type_order <- c("Astrocyte",
+                     "Oligodendrocyte",
+                     "E_Homer2_Pou3f1",
+                     "I_Npy_Lhx6",
+                     "E_Tbr1_Id2",
+                     "Endothelia",
+                     "E_Rora_Zic1",
+                     "I_Chrm2_Pvalb",
+                     "Oligodendrocyte progenitor cell",
+                     "Microglia",
+                     "E_Tshz2_Lamp5",
+                     "Hybrid_EI_neurons",
+                     "E_Cck_Tcf4",
+                     "E_Pou3f1_Cck",
+                     "E_Zic1_Lef1",
+                     "E_Baiap3_Tmem163",
+                     "E_Zic1_Syt9",
+                     "Choroid plexus",
+                     "Ependymal",
+                     "I_Ppp1r1b_Gpr88")
+
+# make per sample heatmaps
 
 for (id in sample_ids) {
   
   print(id)
   
   cell_connect <- cell_connections[[id]]
-
+  
   # sum up cell 2 cell edge counts
   cell_type_connect_total <- ddply(cell_connect,
                                    .(Cell.Type_from,Cell.Type_to),
@@ -240,57 +230,26 @@ for (id in sample_ids) {
   
   pdf(paste0(out_dir, id, ".cell_type_adj_matrix.pdf"), width=8, height=7)
   print(Heatmap(connect_matrix,
-          col=colorRamp2(c(-1.5,0,1.5), c("blue","white","red")),
-          name = "Normalized\nEdge Count",
-          column_title=id))
+                col=colorRamp2(c(-1.5,0,1.5), c("blue","white","red")),
+                name = "Normalized\nEdge Count",
+                column_title=id,
+                cluster_rows = F, cluster_columns = F,
+                row_order = cell_type_order,
+                column_order = cell_type_order))
   dev.off()
   
 }
 
-# one big adjacency matrix
-cell_connections_big <- bind_rows(cell_connections)
+# make per condition heatmaps
+conditions <- unique(cell_metadata$Condition)
 
-cell_type_connect_big <- ddply(cell_connections_big,
-                                 .(Cell.Type_from,Cell.Type_to),
-                                 summarise,
-                                 cell_count=length(from))  
-
-cell_type_connect_big$scaled_count <- scale(log(cell_type_connect_big$cell_count))
-
-connect_big_matrix <- acast(cell_type_connect_big,
-                            Cell.Type_from ~ Cell.Type_to,
-                            value.var = "scaled_count")
-
-pdf(paste0(out_dir, "all_samples.cell_type_adj_matrix.pdf"), width=8, height=7)
-print(Heatmap(connect_big_matrix,
-              col=colorRamp2(c(-1.5,0,1.5), c("blue","white","red")),
-              name = "Normalized\nEdge Count",
-              column_title="All Samples"))
-dev.off()
-
-
-
-
-
-
-
-
-
-
-# filter out astrocytes and oligodendrocytes
-
-filter_ao <- c("Astrocyte", "Oligodendrocyte")
-
-# create adjacency matrices
-
-for (id in sample_ids) {
+for (cond in conditions) {
   
-  print(id)
+  print(cond)
   
-  cell_connect <- cell_connections[[id]]
+  cond_ids <- unique(cell_metadata[cell_metadata$Condition == cond,]$sample_cond)
   
-  cell_connect <- cell_connect[!cell_connect$Cell.Type_from %in% filter_ao &
-                                 !cell_connect$Cell.Type_to %in% filter_ao,]
+  cell_connect <- bind_rows(cell_connections[cond_ids])
   
   # sum up cell 2 cell edge counts
   cell_type_connect_total <- ddply(cell_connect,
@@ -305,36 +264,20 @@ for (id in sample_ids) {
                           Cell.Type_from ~ Cell.Type_to,
                           value.var = "scaled_count")
   
-  pdf(paste0(out_dir, id, ".no_astro_oligo.cell_type_adj_matrix.pdf"), width=8, height=7)
+  
+  pdf(paste0(out_dir, cond, ".cell_type_adj_matrix.pdf"), width=8, height=7)
   print(Heatmap(connect_matrix,
                 col=colorRamp2(c(-1.5,0,1.5), c("blue","white","red")),
                 name = "Normalized\nEdge Count",
-                column_title=id))
+                column_title=cond,
+                cluster_rows = F, cluster_columns = F,
+                row_order = cell_type_order,
+                column_order = cell_type_order))
   dev.off()
   
 }
 
-# one big adjacency matrix
-cell_connections_big <- bind_rows(cell_connections)
 
-cell_connections_big <- cell_connections_big[!cell_connections_big$Cell.Type_from %in% filter_ao &
-                               !cell_connections_big$Cell.Type_to %in% filter_ao,]
 
-cell_type_connect_big <- ddply(cell_connections_big,
-                               .(Cell.Type_from,Cell.Type_to),
-                               summarise,
-                               cell_count=length(from))  
 
-cell_type_connect_big$scaled_count <- scale(log(cell_type_connect_big$cell_count))
-
-connect_big_matrix <- acast(cell_type_connect_big,
-                            Cell.Type_from ~ Cell.Type_to,
-                            value.var = "scaled_count")
-
-pdf(paste0(out_dir, "all_samples.no_astro_oligo.cell_type_adj_matrix.pdf"), width=8, height=7)
-print(Heatmap(connect_big_matrix,
-              col=colorRamp2(c(-1.5,0,1.5), c("blue","white","red")),
-              name = "Normalized\nEdge Count",
-              column_title="All Samples"))
-dev.off()
 
